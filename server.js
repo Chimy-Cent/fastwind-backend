b96a494d0cb25b81c926fad0fb295823ab250127
chimy-cent <chimyinnocent@gmail.com>
2024-04-23 12:01:20 +0100

    fist commit

---
+++ server.js
@@ -0,0 +1,25 @@
+import express from 'express';
+const app = express();
+import bodyParser from 'body-parser';
+import cors from 'cors'
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use(express.static('public'));
+
+app.post('/submit-form', (req, res) => {
+  const { details } = req.body;
+
+  if (details === 'fs123445') {
+    res.json({ message: 'Your parcel is already dispatched and is on its way to you' });
+  } else {
+    res.status(400).json({ error: 'Invalid details' });
+  }
+});
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`); 
+});
